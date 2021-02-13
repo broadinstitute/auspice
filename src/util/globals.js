8c5a31949f6c93e31ee386fba588e6a388ccff9c
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2021-02-12 19:57:36 -0500

    switch to Broad mapbox api key (#6)
    switch to Broad mapbox api key

--- src/util/globals.js
+++ src/util/globals.js
@@ -217,8 +217,8 @@ export const getMapTilesSettings = () => {
   if (hasExtension("mapTiles")) {
     return getExtension("mapTiles");
   }
-  /* defaults */
-  const api = 'https://api.mapbox.com/styles/v1/trvrb/ciu03v244002o2in5hlm3q6w2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHJ2cmIiLCJhIjoiY2tqcnM5bXIxMWV1eTJzazN2YXVrODVnaiJ9.7iPttR9a_W7zuYlUCfrz6A';
+  /* defaults */  
+  const api = 'https://api.mapbox.com/styles/v1/broad-viral/ckl2hdlt80kf717n4mswirdul/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJvYWQtdmlyYWwiLCJhIjoiY2tsMmNrdzMwMDg4MjJ1cGttZXZuamZiayJ9.522Kq9FakoBAD72axEcK2A';
   return {
     api,
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <a style="font-weight: 700" href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>',
